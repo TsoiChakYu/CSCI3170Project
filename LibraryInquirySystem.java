e59846ab6d88cc3820bb0d5319745f6309a73c1f
TSOIChakYu <chakyutsoi@gmail.com>
2021-11-26 20:35:49 +0800

    completed loadData()
    finished loadData()
    please try to see if any new problems occured

--- LibraryInquirySystem.java
+++ LibraryInquirySystem.java
@@ -11,6 +11,8 @@ import java.sql.*;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.File;
+import java.io.FileNotFoundException;
 /**
  *
  * @author 
@@ -141,11 +143,12 @@ class Administrator extends User{
     }
     
     private void createTable(){
-        System.out.println("createTable()");
+        //System.out.println("createTable()");
         System.out.print("Processing...");
         
         try {
             Statement stmt = ConnectDatabase.getConn().createStatement();
+            //drop the same table if existed
             stmt.executeUpdate("DROP TABLE IF EXISTS borrow;");  
             stmt.executeUpdate("DROP TABLE IF EXISTS libuser;");
             stmt.executeUpdate("DROP TABLE IF EXISTS user_category;");        
@@ -153,16 +156,15 @@ class Administrator extends User{
             stmt.executeUpdate("DROP TABLE IF EXISTS copy;");
             stmt.executeUpdate("DROP TABLE IF EXISTS book;");
             stmt.executeUpdate("DROP TABLE IF EXISTS book_category;");
-            // stmt.executeUpdate("CREATE TABLE category ();");
+            //create the tables
             stmt.execute("CREATE TABLE user_category (ucid INTEGER NOT NULL PRIMARY KEY, max INTEGER NOT NULL,  period INTEGER NOT NULL);");
-            stmt.execute("CREATE TABLE libuser (libuid CHAR(10) NOT NULL PRIMARY KEY, name VARCHAR(25) NOT NULL, address VARCHAR(100) NOT NULL, ucid INTEGER NOT NULL, FOREIGN KEY(ucid) REFERENCES user_category(ucid));");
+            stmt.execute("CREATE TABLE libuser (libuid CHAR(10) NOT NULL PRIMARY KEY, name VARCHAR(25) NOT NULL, age INTEGER NOT NULL, address VARCHAR(100) NOT NULL, ucid INTEGER NOT NULL, FOREIGN KEY(ucid) REFERENCES user_category(ucid));");
             stmt.execute("CREATE TABLE book_category (bcid INTEGER NOT NULL PRIMARY KEY, bcname VARCHAR(30) NOT NULL);");
             stmt.execute("CREATE TABLE book (callnum VARCHAR(8) NOT NULL PRIMARY KEY, title VARCHAR(30) NOT NULL, publish DATE, rating FLOAT, tborrowed INTEGER NOT NULL, bcid INTEGER, FOREIGN KEY(bcid) REFERENCES book_category(bcid));"); // date format dd/mm/yy
             stmt.execute("CREATE TABLE copy (callnum VARCHAR(8) NOT NULL, copynum INTEGER NOT NULL, PRIMARY KEY (callnum, copynum), FOREIGN KEY (callnum) REFERENCES book(callnum));");
-           // stmt.execute("CREATE TABLE borrow (libuid CHAR(10) NOT NULL, );"):
             stmt.execute("CREATE TABLE borrow (libuid CHAR(10) NOT NULL, callnum VARCHAR(8) NOT NULL, copynum INTEGER NOT NULL, checkout DATE NOT NULL, returndate DATE, PRIMARY KEY(libuid, callnum, copynum, checkout), FOREIGN KEY(libuid) REFERENCES libuser(libuid), FOREIGN KEY(callnum, copynum) REFERENCES copy(callnum, copynum));");
             stmt.execute("CREATE TABLE authorship(aname VARCHAR(25) NOT NULL, callnum VARCHAR(8) NOT NULL, PRIMARY KEY(aname, callnum), FOREIGN KEY(callnum) REFERENCES book(callnum));");
-            //stmt.execute("show")
+            //close statement
             stmt.close();
             System.out.println("Done. Database is initialized.");
         } catch (Exception e){
@@ -195,17 +197,187 @@ class Administrator extends User{
         //TODO
         try{
             String folder_path;
+            boolean missingFile = false;
+            boolean failInsertData = false;
             System.out.println("");             //a blank line according to the demo provided
             System.out.print("Please enter the folder path:");
             folder_path = scan.next();
-            System.out.println(folder_path);    //for debug
             System.out.print("Processing...");
-            Statement stmt = ConnectDatabase.getConn().createStatement();
+            //check book_category.txt exist
+            try{
+                File bookCategory = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/book_category.txt");
+                Scanner scanFileData = new Scanner(bookCategory);
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.print("\n[Error]: book_category.txt not found. Please make sure you have inputed the correct folder path.");
+                missingFile = true;
+            }
+            //check book.txt exist
+            try{
+                File book = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/book.txt");
+                Scanner scanFileData = new Scanner(book);
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.print("\n[Error]: book.txt not found. Please make sure you have inputed the correct folder path.");
+                missingFile = true;
+            }            
+            //check check_out.txt exist
+            try{
+                File checkOut = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/check_out.txt");
+                Scanner scanFileData = new Scanner(checkOut);
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.print("\n[Error]: check_out.txt not found. Please make sure you have inputed the correct folder path.");
+                missingFile = true;
+            }
+            //check user_category.txt exist
+            try{
+                File userCategory = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/user_category.txt");
+                Scanner scanFileData = new Scanner(userCategory);
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.print("\n[Error]: user_category.txt not found. Please make sure you have inputed the correct folder path.");
+                missingFile = true;
+            }
+            //check user.txt exist
+            try{
+                File userData = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/user.txt");
+                Scanner scanFileData = new Scanner(userData);
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.print("\n[Error]: user.txt not found. Please make sure you have inputed the correct folder path.");
+                missingFile = true;
+            }
+            //return if there is any file missing
+            if(missingFile){
+                System.out.println("");
+                return;
+            }
+            //load book_category data
+            try{
+                File bookCategory = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/book_category.txt");
+                Scanner scanFileData = new Scanner(bookCategory);
+                Statement stmt = ConnectDatabase.getConn().createStatement();
+                while(scanFileData.hasNextLine()){
+                    String[] bookCategoryData = scanFileData.nextLine().split("\\t");
+                    stmt.executeUpdate("INSERT INTO book_category VALUES('" + Integer.parseInt(bookCategoryData[0]) + "',\"" + bookCategoryData[1] + "\");");
+                }
+                stmt.close();
+                scanFileData.close();                
+            }catch(Exception e){
+                System.out.println("[Error]: Fail to insert data to book_category." + e.toString());
+                failInsertData = true;
+            }
+            //load user_category data
+            try{
+                File userCategory = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/user_category.txt");
+                Scanner scanFileData = new Scanner(userCategory);
+                Statement stmt = ConnectDatabase.getConn().createStatement();
+                while(scanFileData.hasNextLine()){
+                    String[] userCategoryData = scanFileData.nextLine().split("\\t");
+                    stmt.executeUpdate("INSERT INTO user_category VALUES('" + Integer.parseInt(userCategoryData[0]) + "','" + Integer.parseInt(userCategoryData[1]) + "','" + Integer.parseInt(userCategoryData[2]) + "');");
+                }
+                stmt.close();                
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.println("[Error]: Fail to insert data to user_category." + e.toString());
+                failInsertData = true;
+            }
+            //Load libuser data
+            try{
+                File userData = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/user.txt");
+                Scanner scanFileData = new Scanner(userData);
+                Statement stmt = ConnectDatabase.getConn().createStatement();
+                while(scanFileData.hasNextLine()){
+                    String[] libuserData = scanFileData.nextLine().split("\\t");
+                    stmt.executeUpdate("INSERT INTO libuser VALUES(\"" + libuserData[0] + "\",\"" + libuserData[1] + "\",'" + Integer.parseInt(libuserData[2]) + "',\"" + libuserData[3] + "\",'" + Integer.parseInt(libuserData[4]) +"');");
+                }
+                stmt.close();                 
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.println("[Error]: Fail to insert data to libuser." + e.toString());
+                failInsertData = true;
+            }
+            //Load book
+            try{
+                File book = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/book.txt");
+                Scanner scanFileData = new Scanner(book);
+                Statement stmt = ConnectDatabase.getConn().createStatement();
+                while(scanFileData.hasNextLine()){
+                    String[] bookData = scanFileData.nextLine().split("\\t");
+                    if(bookData[4].equals("null")){
+                        stmt.executeUpdate("INSERT INTO book VALUES(\"" + bookData[0] + "\",\"" + bookData[2] + "\",\"" + bookData[4] + "\"," + (bookData[5].equals("null")?"null":Float.parseFloat(bookData[5])) + ",'" + Integer.parseInt(bookData[6]) + "','" + Integer.parseInt(bookData[7]) +"');");   //can insert without '', will show warning if insert 'null'/ "null" instead of null
+                    }else{
+                        stmt.executeUpdate("INSERT INTO book VALUES(\"" + bookData[0] + "\",\"" + bookData[2] + "\",STR_TO_DATE('" + bookData[4] + "', '%d/%m/%Y')," + (bookData[5].equals("null")?"null":Float.parseFloat(bookData[5])) + ",'" + Integer.parseInt(bookData[6]) + "','" + Integer.parseInt(bookData[7]) +"');");
+                    }
+                }
+                stmt.close();                
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.println("[Error]: Fail to insert data to book." + e.toString());
+                failInsertData = true;
+            }  
+            //Load authorship 
+            try{
+                File book = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/book.txt");
+                Scanner scanFileData = new Scanner(book);
+                Statement stmt = ConnectDatabase.getConn().createStatement();
+                while(scanFileData.hasNextLine()){
+                    String[] tmpData = scanFileData.nextLine().split("\\t");
+                    String[] authorshipData = tmpData[3].split(",");
+                    for(String author : authorshipData){
+                        stmt.executeUpdate("INSERT INTO authorship VALUES(\"" + author +"\",\"" + tmpData[0] + "\");");
+                    }
+                }
+                stmt.close();                
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.println("[Error]: Fail to insert data to authorship." + e.toString());
+                failInsertData = true;
+            } 
+            //Load copy
+            try{
+                File book = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/book.txt");
+                Scanner scanFileData = new Scanner(book);
+                Statement stmt = ConnectDatabase.getConn().createStatement();
+                while(scanFileData.hasNextLine()){
+                    String[] tmpData = scanFileData.nextLine().split("\\t");
+                    int tmpCopyNum = Integer.parseInt(tmpData[1]);
+                    for(int i=1; i<=tmpCopyNum; i++){   //??? not sure whether this is correct or not
+                        stmt.executeUpdate("INSERT INTO copy VALUES('" + tmpData[0] + "','" + i + "');");
+                    }
+                }
+                stmt.close();                
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.println("[Error]: Fail to insert data to copy." + e.toString());
+                failInsertData = true;
+            } 
+            //load borrow
+            try{
+                File checkOut = new File(System.getProperty("user.dir") + "/" + folder_path + "" + "/check_out.txt");
+                Scanner scanFileData = new Scanner(checkOut);
+                Statement stmt = ConnectDatabase.getConn().createStatement();
 
-
-
-
-            stmt.close();
+                while(scanFileData.hasNextLine()){
+                    String[] checkOutData = scanFileData.nextLine().split("\\t");
+                    if(checkOutData[4].equals("null")){
+                        stmt.executeUpdate("INSERT INTO borrow VALUES(\"" + checkOutData[2] + "\",\"" + checkOutData[0] + "\",'" + Integer.parseInt(checkOutData[1]) + "',STR_TO_DATE('" + checkOutData[3] + "', '%d/%m/%Y')," + checkOutData[4] + ");");
+                    }else{
+                        stmt.executeUpdate("INSERT INTO borrow VALUES(\"" + checkOutData[2] + "\",\"" + checkOutData[0] + "\",'" + Integer.parseInt(checkOutData[1]) + "',STR_TO_DATE('" + checkOutData[3] + "', '%d/%m/%Y'),STR_TO_DATE('" + checkOutData[4] + "', '%d/%m/%Y'));");
+                    }
+                }
+                stmt.close();                
+                scanFileData.close();
+            }catch(Exception e){
+                System.out.println("[Error]: Fail to insert data to borrow." + e.toString());
+                failInsertData = true;
+            }
+            //return if fail to load any of the data
+            if(failInsertData){
+                return;
+            }
+            //Show the process is done.
             System.out.println("Done. Data is inputted to the database.");
         }catch(Exception e){
             System.out.println("[Error]: "+e.toString() +"Please make sure you have already created the database.");
